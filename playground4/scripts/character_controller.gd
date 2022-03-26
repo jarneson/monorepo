76b7b84c9549904b6daf34b87d9fb138acd8b8f7
Jeff <jeffrey.arneson@gmail.com>
2022-03-26 12:41:11 -0500

    more shader work

---
+++ playground4/scripts/character_controller.gd
@@ -0,0 +1,47 @@
+extends CharacterBody3D
+
+const MOUSE_SENSITIVITY = 0.002
+const SPEED = 15.0
+const JUMP_VELOCITY = 34.5
+
+# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	print("ready")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		$Pivot.rotate_x(-event.relative.y*MOUSE_SENSITIVITY)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
+	if event.is_action_released("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			print("unlock")
+		elif Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			print("lock")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_front", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
