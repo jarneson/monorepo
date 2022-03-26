ef207e13c632d6b63537643d39a57f5e066515af
Jeffrey Arneson <jeffrey.arneson@gmail.com>
2022-03-25 21:05:24 -0500

    godot4 playground work

---
+++ playground4/Position3D.gd
@@ -0,0 +1,9 @@
+extends Position3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate_y(PI * delta / 32.0)
